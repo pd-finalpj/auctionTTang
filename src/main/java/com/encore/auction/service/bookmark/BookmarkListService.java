6cf4b0e074276bcb40651f912eea32ee65842e49
12OneTwo12 <jji042842@gmail.com>
2022-12-06 19:40:29 +0900

    #16 Create Find Bookmark List By User Id service

--- src/main/java/com/encore/auction/service/bookmark/BookmarkListService.java
+++ src/main/java/com/encore/auction/service/bookmark/BookmarkListService.java
@@ -1,12 +1,35 @@
 package com.encore.auction.service.bookmark;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.encore.auction.controller.bookmark.responses.BookmarkDetailsListResponse;
+import com.encore.auction.controller.bookmark.responses.BookmarkDetailsResponse;
+import com.encore.auction.exception.NonExistResourceException;
+import com.encore.auction.model.user.User;
+import com.encore.auction.repository.BookmarkListRetrieveRepository;
+import com.encore.auction.repository.UserRepository;
 
 @Service
 public class BookmarkListService {
+
+	private final UserRepository userRepository;
+	private final BookmarkListRetrieveRepository bookmarkListRetrieveRepository;
+
+	public BookmarkListService(UserRepository userRepository,
+		BookmarkListRetrieveRepository bookmarkListRetrieveRepository) {
+		this.userRepository = userRepository;
+		this.bookmarkListRetrieveRepository = bookmarkListRetrieveRepository;
+	}
+
 	public BookmarkDetailsListResponse retrieveBookmarkListByUserId(String userId) {
-		return null;
+		User user = userRepository.findById(userId)
+			.orElseThrow(() -> new NonExistResourceException("User does not exist"));
+
+		List<BookmarkDetailsResponse> bookmarkDetailsResponseList = bookmarkListRetrieveRepository.retrieveBookmarkListByUserId(
+			user.getId());
+		
+		return new BookmarkDetailsListResponse(userId, bookmarkDetailsResponseList);
 	}
 }
