28f5ee95375eef210511c3b509ac16618a31e8dd
12OneTwo12 <jji042842@gmail.com>
2022-12-07 10:45:37 +0900

    #20 Create Bidding List Service

--- src/main/java/com/encore/auction/service/bidding/bidding/BiddingListService.java
+++ src/main/java/com/encore/auction/service/bidding/bidding/BiddingListService.java
@@ -1,12 +1,35 @@
 package com.encore.auction.service.bidding.bidding;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.encore.auction.controller.bidding.bidding.responses.BiddingDetailsListResponse;
+import com.encore.auction.controller.bidding.bidding.responses.BiddingDetailsResponse;
+import com.encore.auction.exception.NonExistResourceException;
+import com.encore.auction.model.user.User;
+import com.encore.auction.repository.BiddingListRetrieveRepository;
+import com.encore.auction.repository.UserRepository;
 
 @Service
 public class BiddingListService {
+
+	private final UserRepository userRepository;
+	private final BiddingListRetrieveRepository biddingListRetrieveRepository;
+
+	public BiddingListService(UserRepository userRepository,
+		BiddingListRetrieveRepository biddingListRetrieveRepository) {
+		this.userRepository = userRepository;
+		this.biddingListRetrieveRepository = biddingListRetrieveRepository;
+	}
+
 	public BiddingDetailsListResponse retrieveBiddingListByUserId(String userId) {
-		return null;
+		User user = userRepository.findById(userId)
+			.orElseThrow(() -> new NonExistResourceException("User does not exist"));
+
+		List<BiddingDetailsResponse> biddingDetailsResponseList = biddingListRetrieveRepository.retrieveBiddingListByUserId(
+			user.getId());
+
+		return new BiddingDetailsListResponse(user.getId(), biddingDetailsResponseList);
 	}
 }
