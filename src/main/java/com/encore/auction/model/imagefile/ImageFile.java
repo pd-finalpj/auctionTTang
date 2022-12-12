f2a3b30c603701fa83dc1c5fd23bdf701cff46fd
twghy <cine419@naver.com>
2022-12-12 14:44:41 +0900

    #71 Create ImageFileController

--- src/main/java/com/encore/auction/model/imagefile/ImageFile.java
+++ src/main/java/com/encore/auction/model/imagefile/ImageFile.java
@@ -7,10 +7,9 @@ import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-
-import org.springframework.data.annotation.Id;
 
 import com.encore.auction.model.auction.item.AuctionItem;
 
